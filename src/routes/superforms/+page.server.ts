497257bf8e66e264a2f35f1693c66d5a25ff597b
Javier Sánchez Mendoza <javier.sanchezm@sat.gob.mx>
2024-02-08 13:13:03 -0600

    status quo

--- src/routes/superforms/+page.server.ts
+++ src/routes/superforms/+page.server.ts
@@ -1,27 +1,43 @@
-import { z } from 'zod';
 import { superValidate } from 'sveltekit-superforms/server';
+import { z } from 'zod';
+import { fail } from '@sveltejs/kit';
 
-const schema = z.object({
-  name: z.string().default('Hello world!'),
-  email: z.string().email()
+const userSchema = z.object({
+	firstName: z.string().min(1),
+	lastName: z.string().min(1),
+	email: z.string().email(),
+	employeeNumber: z.number().min(1)
 });
 
+export const load = async () => {
+	// Server API:
+	const form = await superValidate(userSchema);
 
+	// Always return { form } in load and form actions.
+	return { form };
+};
+
+// export const actions = {
+// 	default: async (event) => {
+// 		const form = await superValidate(event, userSchema);
+
+// 		console.log(form);
+
+// 		return { form };
+// 	}
+// };
 
 export const actions = {
-  default: async ({ request }) => {
-	const form = await superValidate(request, schema);
-	console.log('POST', form);
+	default: async ({ request }) => {
+		const form = await superValidate(request, userSchema);
+		console.log('POST',form);
 
-	// Convenient validation check:
-	if (!form.valid) {
-	  // Again, return { form } and things will just work.
-	  return fail(400, { form });
+		if (!form.valid) {
+			return fail(400, {
+				form
+			});
+		}
+
+		return { form };
 	}
-
-	// TODO: Do something with the validated form.data
-
-	// Yep, return { form } here too
-	return { form };
-  }
 };
--- src/routes/superforms/+page.svelte
+++ src/routes/superforms/+page.svelte
@@ -1,22 +1,84 @@
 <script lang="ts">
-  import type { PageData } from './$types';
-  import { superForm } from 'sveltekit-superforms/client';
-  import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
+import { superForm } from "sveltekit-superforms/client"
+import SuperDebug from "sveltekit-superforms/client/SuperDebug.svelte"
+import { z } from 'zod';
 
-  export let data: PageData;
 
-  // Client API:
-  const { form } = superForm(data.form);
+export let data;
+
+const userSchema = z.object({
+    firstName: z.string().min(1),
+      lastName: z.string().min(1),
+      email: z.string().email(),
+      employeeNumber: z.number().min(1)
+});
+
+const { form, errors, enhance } = superForm(data.form, {
+  validators: userSchema
+});
+// const { form, errors, enhance, constraints } = superForm(data.form, {
+// 	validators: {
+// 		firstName: (name) => name.length < 3 ? 'Name must be at least 3 characters' : null,
+// 	}
+// });
+
 </script>
 
-<form method="POST">
-  <label for="name">Name</label>
-  <input type="text" name="name" bind:value={$form.name} />
+<SuperDebug data={form}/>
 
-  <label for="email">E-mail</label>
-  <input type="email" name="email" bind:value={$form.email} />
+<div class='form-wrapper'>
+  <form method="POST" use:enhance>
+    <div>
+      <label for="name">First Name</label>
+      <input class="input" title="Input (text)" type="text" name="firstName" bind:value={$form.firstName}/>
+      {#if $errors.firstName}
+        <p class='error-para'>{$errors.firstName}</p>
+      {/if}
+    </div>
 
-  <div><button>Submit</button></div>
-</form>
+    <div>
+      <label for="name">Last Name</label>
+      <input class="input" title="Input (text)" type="text" name="lastName" bind:value={$form.lastName}/>
+      {#if $errors.lastName}
+        <p class='error-para'>{$errors.lastName}</p>
+      {/if}
+    </div>
 
-<SuperDebug data={$form} />
+    <div>
+      <label for="email">E-mail</label>
+      <input class="input" title="Input (text)" type="email" name="email" bind:value={$form.email}/>
+      {#if $errors.email}
+        <p class='error-para'>{$errors.email}</p>
+      {/if}
+    </div>
+
+    <div>
+      <label for="email">Employee Number</label>
+      <input class="input" title="Input (text)" type="number" name="employeeNumber" bind:value={$form.employeeNumber}/>
+      {#if $errors.employeeNumber}
+        <p class='error-para'>{$errors.employeeNumber}</p>
+      {/if}
+    </div>
+    <div><button class="btn variant-filled">Submit</button></div>
+  </form>
+</div>
+
+<style>
+.form-wrapper{
+  display: flex;
+  justify-content: center;
+}
+button {
+    background-color: #008001;
+    border-radius: 5px;
+    border: none;
+    padding: 10px;
+  }
+  button:hover {
+    background-color: #AAD922;
+  }
+  .error-para{
+    color: red;
+    font-size: 12px;
+  }
+</style>
